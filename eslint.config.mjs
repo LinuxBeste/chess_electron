397e395ff2e9fc355ecfd13c09d929066088c802
linuxbeste <linuxbeste@gmail.com>
2026-07-09 14:45:22 +0200

    fix: add chess-api/bin dir to eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**', '**/release/**'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**', '**/release/**', 'chess-api/bin/**'],
   },
 
   js.configs.recommended,
